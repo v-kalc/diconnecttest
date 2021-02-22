b76ca678abb09eea4e52e1954c1337a4fe114c5c
v-kalc <59079664+v-kalc@users.noreply.github.com>
2021-02-22 17:40:35 +0530

    Added

---
+++ Source/Microsoft.Teams.Apps.DIConnect/Cards/ApprovalNotificationCard.cs
@@ -0,0 +1,146 @@
+﻿// <copyright file="ApprovalNotificationCard.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.DIConnect.Cards
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using AdaptiveCards;
+    using Microsoft.Bot.Schema;
+    using Microsoft.Extensions.Localization;
+    using Microsoft.Teams.Apps.DIConnect.Common;
+    using Microsoft.Teams.Apps.DIConnect.Common.Resources;
+    using Microsoft.Teams.Apps.DIConnect.Constants;
+
+    /// <summary>
+    /// Class that helps to return approval notification card as attachment.
+    /// </summary>
+    public static class ApprovalNotificationCard
+    {
+        /// <summary>
+        /// Get approval notification card attachment.
+        /// </summary>
+        /// <param name="localizer">The current culture's string localizer.</param>
+        /// <param name="applicationBasePath">Represents the Application base URI.</param>
+        /// <returns>Approval notification card attachment.</returns>
+        public static Attachment GetCard(IStringLocalizer<Strings> localizer, string applicationBasePath)
+        {
+            AdaptiveCard card = new AdaptiveCard(new AdaptiveSchemaVersion(CardConstants.AdaptiveCardVersion))
+            {
+                Body = new List<AdaptiveElement>
+                {
+                    new AdaptiveColumnSet
+                    {
+                        Columns = new List<AdaptiveColumn>
+                        {
+                            new AdaptiveColumn
+                            {
+                                Width = "12",
+                                Items = new List<AdaptiveElement>
+                                {
+                                    new AdaptiveTextBlock
+                                    {
+                                        Text = localizer.GetString("RequestSubmittedText"),
+                                        Weight = AdaptiveTextWeight.Bolder,
+                                        Size = AdaptiveTextSize.Large,
+                                    },
+                                },
+                            },
+                            new AdaptiveColumn
+                            {
+                                Width = "3",
+                                Items = new List<AdaptiveElement>
+                                {
+                                    new AdaptiveImage
+                                    {
+                                        AltText = "not available",
+                                    },
+                                },
+                            },
+                        },
+                    },
+                    new AdaptiveTextBlock
+                    {
+                        Text = "test the description",
+                        Spacing = AdaptiveSpacing.Small,
+                        Wrap = true,
+                    },
+                    GetAdaptiveCardColumnSet(localizer.GetString("NameText"), "test"),
+                    GetAdaptiveCardColumnSet(localizer.GetString("TagText"), "test"),
+                    GetAdaptiveCardColumnSet(localizer.GetString("LocationText"), "test"),
+                    new AdaptiveTextBlock
+                    {
+                        Size = AdaptiveTextSize.Medium,
+                        Separator = true,
+                    },
+                    GetAdaptiveCardColumnSet(localizer.GetString("SearchEnabledText"), "test"),
+                },
+            };
+
+            if (true)
+            {
+                card.Actions.Add(
+                    new AdaptiveSubmitAction()
+                    {
+                        Title = localizer.GetString("ApproveButtonText"),
+                    });
+
+                card.Actions.Add(
+                    new AdaptiveSubmitAction()
+                    {
+                        Title = localizer.GetString("RejectButtonText"),
+                    });
+            }
+
+            return new Attachment
+            {
+                ContentType = AdaptiveCard.ContentType,
+                Content = card,
+            };
+        }
+
+        /// <summary>
+        /// Get adaptive card column set.
+        /// </summary>
+        /// <param name="title">Column title.</param>
+        /// <param name="value">Column value.</param>
+        /// <returns>AdaptiveColumnSet.</returns>
+        private static AdaptiveColumnSet GetAdaptiveCardColumnSet(string title, string value)
+        {
+            return new AdaptiveColumnSet
+            {
+                Columns = new List<AdaptiveColumn>
+                {
+                    new AdaptiveColumn
+                    {
+                        Width = "50",
+                        Items = new List<AdaptiveElement>
+                        {
+                            new AdaptiveTextBlock
+                            {
+                                Text = $"{title}:",
+                                Wrap = true,
+                                Weight = AdaptiveTextWeight.Bolder,
+                                Size = AdaptiveTextSize.Medium,
+                            },
+                        },
+                    },
+                    new AdaptiveColumn
+                    {
+                        Width = "100",
+                        Items = new List<AdaptiveElement>
+                        {
+                            new AdaptiveTextBlock
+                            {
+                                Text = value,
+                                Wrap = true,
+                            },
+                        },
+                    },
+                },
+            };
+        }
+    }
+}
---
+++ Source/Microsoft.Teams.Apps.DIConnect/Cards/WelcomeCard.cs
@@ -0,0 +1,111 @@
+﻿// <copyright file="WelcomeCard.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Apps.DIConnect.Cards
+{
+    using System;
+    using System.Collections.Generic;
+    using AdaptiveCards;
+    using Microsoft.Bot.Schema;
+    using Microsoft.Extensions.Localization;
+    using Microsoft.Teams.Apps.DIConnect.Common;
+    using Microsoft.Teams.Apps.DIConnect.Common.Resources;
+    using Microsoft.Teams.Apps.DIConnect.Constants;
+    using Microsoft.Teams.Apps.DIConnect.Models;
+
+    /// <summary>
+    /// Class that helps to return welcome card as attachment.
+    /// </summary>
+    public static class WelcomeCard
+    {
+        /// <summary>
+        /// Get welcome card attachment to show on Microsoft Teams personal scope.
+        /// </summary>
+        /// <param name="localizer">The current culture's string localizer.</param>
+        /// <param name="applicationManifestId">Application manifest id.</param>
+        /// <returns>End user welcome card attachment.</returns>
+        public static Attachment GetEnduserCard(IStringLocalizer<Strings> localizer, string applicationManifestId)
+        {
+            AdaptiveCard card = new AdaptiveCard(new AdaptiveSchemaVersion(CardConstants.AdaptiveCardVersion))
+            {
+                Body = new List<AdaptiveElement>
+                {
+                    new AdaptiveTextBlock
+                    {
+                        Text = localizer.GetString("WelcomeTitleText"),
+                        Weight = AdaptiveTextWeight.Bolder,
+                        Size = AdaptiveTextSize.Large,
+                    },
+                    new AdaptiveTextBlock
+                    {
+                        Text = localizer.GetString("WelcomeHeaderText"),
+                        Spacing = AdaptiveSpacing.None, 
+                        Wrap = true,
+                    },
+                    new AdaptiveTextBlock
+                    {
+                        Text = localizer.GetString("WelcomeSubHeaderText"),
+                        Spacing = AdaptiveSpacing.None,
+                        Wrap = true,
+                    },
+                    new AdaptiveTextBlock
+                    {
+                        Text = "* " + localizer.GetString("DiscoverGroupBulletPoint"),
+                        Wrap = true,
+                        Spacing = AdaptiveSpacing.None,
+                    },
+                    new AdaptiveTextBlock
+                    {
+                        Text = "* " + localizer.GetString("QueriesBulletPoint"),
+                        Wrap = true,
+                        Spacing = AdaptiveSpacing.None,
+                    },
+                    new AdaptiveTextBlock
+                    {
+                        Text = "* " + localizer.GetString("ConnectPeopleBulletPoint"),
+                        Wrap = true,
+                        Spacing = AdaptiveSpacing.None,
+                    },
+                    new AdaptiveTextBlock
+                    {
+                        Text = "* " + localizer.GetString("BroadcastMessageBulletPoint"),
+                        Wrap = true,
+                        Spacing = AdaptiveSpacing.None,
+                    },
+                    new AdaptiveTextBlock
+                    {
+                        Text = localizer.GetString("WelcomeContentText"),
+                        Wrap = true,
+                    },
+                },
+
+                Actions = new List<AdaptiveAction>
+                {
+                    new AdaptiveOpenUrlAction
+                    {
+                        Title = localizer.GetString("DiscoverGroupButtonText"),
+                        Url = new Uri($"{DeepLinkConstants.TabBaseRedirectURL}/{applicationManifestId}/{CardConstants.DiscoverGroupTabEntityId}"),
+                    },
+                    new AdaptiveSubmitAction
+                    {
+                        Title = "Pause",
+                        Data = new AdaptiveSubmitActionData
+                        {
+                            Msteams = new CardAction
+                            {
+                                Type = CardConstants.FetchActionType,
+                            },
+                        },
+                    },
+                },
+            };
+
+            return new Attachment
+            {
+                ContentType = AdaptiveCard.ContentType,
+                Content = card,
+            };
+        }
+    }
+}
